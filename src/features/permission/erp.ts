0220dbe1c81d38e2b4b7141a3df88bf1dc85b757
JohnnyBoySou <joaodesousa101@gmail.com>
2025-11-10 18:12:45 -0300

    add new permissions and organize

---
+++ src/features/permission/erp.ts
@@ -0,0 +1,140 @@
+export const ERP_PERMISSIONS = [
+
+    //CATEGORIES
+    {
+        resource: 'CATEGORIES',
+        action: 'READ',
+        name: 'Visualizar Categorias',
+        description: 'Permite visualizar categorias',
+    },
+    {
+        resource: 'CATEGORIES',
+        action: 'UPDATE',
+        name: 'Atualizar Categorias',
+        description: 'Permite atualizar categorias',
+    },
+    {
+        resource: 'CATEGORIES',
+        action: 'CREATE',
+        name: 'Criar Categorias',
+        description: 'Permite criar categorias',
+    },
+    {
+        resource: 'CATEGORIES',
+        action: 'DELETE',
+        name: 'Deletar Categorias',
+        description: 'Permite deletar categorias',
+    },
+
+    //SUPPLIERS
+    {
+        resource: 'SUPPLIERS',
+        action: 'READ',
+        name: 'Visualizar Fornecedores',
+        description: 'Permite visualizar fornecedores',
+    },
+    {
+        resource: 'SUPPLIERS',
+        action: 'UPDATE',
+        name: 'Atualizar Fornecedores',
+        description: 'Permite atualizar fornecedores',
+    },
+    {
+        resource: 'SUPPLIERS',
+        action: 'CREATE',
+        name: 'Criar Fornecedores',
+        description: 'Permite criar fornecedores',
+    },
+    {
+        resource: 'SUPPLIERS',
+        action: 'DELETE',
+        name: 'Deletar Fornecedores',
+        description: 'Permite deletar fornecedores',
+    },
+
+    //MOVEMENTS
+    {
+        resource: 'MOVEMENTS',
+        action: 'READ',
+        name: 'Visualizar Movimentos',
+        description: 'Permite visualizar movimentos',
+    },
+    {
+        resource: 'MOVEMENTS',
+        action: 'UPDATE',
+        name: 'Atualizar Movimentos',
+        description: 'Permite atualizar movimentos',
+    },
+    {
+        resource: 'MOVEMENTS',
+        action: 'CREATE',
+        name: 'Criar Movimentos',
+        description: 'Permite criar movimentos',
+    },
+    {
+        resource: 'MOVEMENTS',
+        action: 'DELETE',
+        name: 'Deletar Movimentos',
+        description: 'Permite deletar movimentos',
+    },
+
+    // PRODUCTS
+    {
+        resource: 'PRODUCTS',
+        action: 'CREATE',
+        name: 'Criar Produto',
+        description: 'Permite criar novos produtos',
+    },
+    {
+        resource: 'PRODUCTS',
+        action: 'READ',
+        name: 'Visualizar Produtos',
+        description: 'Permite visualizar produtos',
+    },
+    {
+        resource: 'PRODUCTS',
+        action: 'UPDATE',
+        name: 'Atualizar Produto',
+        description: 'Permite atualizar produtos',
+    },
+    {
+        resource: 'PRODUCTS',
+        action: 'DELETE',
+        name: 'Deletar Produto',
+        description: 'Permite deletar produtos',
+    },
+    {
+        resource: 'PRODUCTS',
+        action: 'MANAGE',
+        name: 'Gerenciar Produtos',
+        description: 'Permite gerenciar completamente produtos',
+    },
+    {
+        resource: 'PRODUCTS',
+        action: 'IMPORT',
+        name: 'Importar Produtos',
+        description: 'Permite importar produtos',
+    },
+    {
+        resource: 'PRODUCTS',
+        action: 'EXPORT',
+        name: 'Exportar Produtos',
+        description: 'Permite exportar produtos',
+    },
+
+
+    // REPORTS
+    {
+        resource: 'REPORTS',
+        action: 'READ',
+        name: 'Visualizar Relatórios',
+        description: 'Permite visualizar relatórios',
+    },
+    {
+        resource: 'REPORTS',
+        action: 'EXPORT',
+        name: 'Exportar Relatórios',
+        description: 'Permite exportar relatórios',
+    },
+
+]
---
+++ src/features/permission/wfm.ts
@@ -0,0 +1,107 @@
+export const WFM_PERMISSIONS = [
+
+    // SHIFTS
+    {
+        resource: 'SHIFTS',
+        action: 'READ',
+        name: 'Visualizar Escalas',
+        description: 'Permite visualizar escalas',
+    },
+    {
+        resource: 'SHIFTS',
+        action: 'UPDATE',
+        name: 'Atualizar Escala',
+        description: 'Permite atualizar escalas',
+    },
+    {
+        resource: 'SHIFTS',
+        action: 'CREATE',
+        name: 'Criar Escala',
+        description: 'Permite criar escalas',
+    },
+    {
+        resource: 'SHIFTS',
+        action: 'DELETE',
+        name: 'Deletar Escala',
+        description: 'Permite deletar escalas',
+    },
+
+    // SPACES
+    {
+        resource: 'SPACES',
+        action: 'READ',
+        name: 'Visualizar Espaços',
+        description: 'Permite visualizar espaços',
+    },
+    {
+        resource: 'SPACES',
+        action: 'UPDATE',
+        name: 'Atualizar Espaço',
+        description: 'Permite atualizar espaços',
+    },
+    {
+        resource: 'SPACES',
+        action: 'CREATE',
+        name: 'Criar Espaço',
+        description: 'Permite criar espaços',
+    },
+    {
+        resource: 'SPACES',
+        action: 'DELETE',
+        name: 'Deletar Espaço',
+        description: 'Permite deletar espaços',
+    },
+
+    // SCHEDULES
+    {
+        resource: 'SCHEDULES',
+        action: 'READ',
+        name: 'Visualizar Agendamentos',
+        description: 'Permite visualizar agendamentos',
+    },
+    {
+        resource: 'SCHEDULES',
+        action: 'UPDATE',
+        name: 'Atualizar Agendamento',
+        description: 'Permite atualizar agendamentos',
+    },
+    {
+        resource: 'SCHEDULES',
+        action: 'CREATE',
+        name: 'Criar Agendamento',
+        description: 'Permite criar agendamentos',
+    },
+    {
+        resource: 'SCHEDULES',
+        action: 'DELETE',
+        name: 'Deletar Agendamento',
+        description: 'Permite deletar agendamentos',
+    },
+
+    // SCHEDULE OCCURRENCES
+    {
+        resource: 'SCHEDULE_OCCURRENCES',
+        action: 'READ',
+        name: 'Visualizar Ocorrências de Agendamento',
+        description: 'Permite visualizar ocorrências de agendamentos',
+    },
+    {
+        resource: 'SCHEDULE_OCCURRENCES',
+        action: 'UPDATE',
+        name: 'Atualizar Ocorrência de Agendamento',
+        description: 'Permite atualizar ocorrências de agendamentos',
+    },
+    {
+        resource: 'SCHEDULE_OCCURRENCES',
+        action: 'CREATE',
+        name: 'Criar Ocorrência de Agendamento',
+        description: 'Permite criar ocorrências de agendamentos',
+    },
+    {
+        resource: 'SCHEDULE_OCCURRENCES',
+        action: 'DELETE',
+        name: 'Deletar Ocorrência de Agendamento',
+        description: 'Permite deletar ocorrências de agendamentos',
+    },
+
+]
